9fb9714519a578438fa33cdb48c7c0503adaac0e
fernando <fernando.csconceicao@outlook.com>
2020-06-27 22:20:31 -0300

    Rest Testado

---
+++ src/main/java/com/ti89/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.ti89.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	@RequestMapping(method= RequestMethod.GET)
+	public String listar() {
+		 return ("O metodo está funcionando");
+	}
+	
+
+}
