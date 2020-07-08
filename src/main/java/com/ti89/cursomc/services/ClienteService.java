b6a198d3d789da0e03654e602a093317c3c0e6ad
fernando <fernando.csconceicao@outlook.com>
2020-07-08 00:46:39 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/ti89/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.ti89.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ti89.cursomc.domain.Cliente;
+import com.ti89.cursomc.repositories.ClienteRepository;
+import com.ti89.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+		
+		@Autowired
+		private ClienteRepository repo;
+		
+		
+		public Cliente buscar(Integer id) {
+			Optional<Cliente> obj = repo.findById(id);
+			
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: "
+					+ Cliente.class.getName()
+					));
+		}
+		
+
+}
